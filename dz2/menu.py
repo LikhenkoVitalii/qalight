33a394e2d1d6213ee2157949a3401ab7a28ec0b9
Likhenko Vitalii <gotrein0@gmail.com>
2020-03-03 20:26:29 +0200

    5

--- dz2/menu.py
+++ dz2/menu.py
@@ -2,30 +2,41 @@ import sys
 
 
 def menu():
+
     print("\n 1.Создать запись\n 2.Найти запись\n 3.Редактировать запись\n 4.Удалить запись\n 5.Выйти из программы\n")
     print("Введите значение от 1 до 5:")
     get = input()
 
-    if get == '1':
-        print("Создать запись")
-        pass
+    while get:
 
-    elif get == '2':
-        print("Найти запись")
-        pass
+            if get == '1':
+                print("Создать запись")
+                pass
+                break
 
-    elif get == '3':
-        print("Редактировать запись")
-        pass
+            elif get == '2':
+                print("Найти запись")
+                pass
+                break
 
-    elif get == '4':
-        print("Удалить запись")
-        pass
+            elif get == '3':
+                print("Редактировать запись")
+                pass
+                break
 
-    elif get == '5':
-        sys.exit
+            elif get == '4':
+                print("Удалить запись")
+                pass
+                break
 
-    else: print("Неверный ввод")
+            elif get == '5':
+                 sys.exit()
+
+            else:
+                print("Неверный ввод")
+                print("Введите значение от 1 до 5:")
+                get = input()
+
 
 
 menu()
